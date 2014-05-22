d8554a0bbb247534a12fd715f910f3c21c0b6b7c
martincz <martincz@mokeedev.com>
2014-05-22 18:00:21 +0800

    mokee bringup
    Change-Id: Id784f9233621cde3b0e89b0e7749ebd340dda150

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := l900
@@ -8,15 +8,15 @@ PRODUCT_RELEASE_NAME := l900
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/l900/full_l900.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := l900
-PRODUCT_NAME := cm_l900
+PRODUCT_NAME := mk_l900
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SPH-L900
 PRODUCT_MANUFACTURER := samsung
